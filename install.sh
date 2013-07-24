2630832f2fa48633c295068cdad73b8cdcc5f790
soasme@gmail.com <soasme@gmail.com>
2013-07-24 15:27:05 +0800

    Stupid way to download colortheme

--- install.sh
+++ install.sh
@@ -5,16 +5,9 @@ echo "installing Vundle"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 ln -s ~/.vim/vimrc ~/.vimrc
 
-mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/undo
 mkdir -p ~/.vim/swaps
 mkdir -p ~/.vim/backups
-cd ~/.vim/colors
-
-curl https://raw.github.com/tpope/vim-vividchalk/master/colors/vividchalk.vim > vividchalk.vim
-curl https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim > solarized.vim
-curl https://raw.github.com/garybernhardt/dotfiles/master/.vim/colors/grb256.vim > grb256.vim
-curl https://raw.github.com/sickill/vim-monokai/master/colors/Monokai.vim > Monokai.vim
 
 echo "installing plugin using Vundle"
 vim +BundleInstall! +BundleClean +q
